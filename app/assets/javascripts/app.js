0d13db6729ccc02a522e27bbcd99662cc2df48c0
quintencls <quintencls@gmail.com>
2016-09-06 09:04:32 +0200

    Setup angular app with controller and template

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,16 @@
-demo = angular.module('demo',[])
+app = angular.module('demoapp',[
+  'templates',
+  'ngRoute',
+  'controllers',
+])
+
+app.config([ '$routeProvider',function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'index.html',
+        controller: 'ArticlesController'
+      })
+  }
+])
+
+controllers = angular.module('controllers', [])
